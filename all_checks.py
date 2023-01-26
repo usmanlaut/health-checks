bbc5b31aaab45cd2eb85ce61a6b578e0b808c687
My name <me@example.com>
2023-01-26 16:24:53 +0800

    Rename if statement

--- all_checks.py
+++ all_checks.py
@@ -6,14 +6,14 @@ def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exist("/run/reboot-required")
 
-def check_disk_full(disk, min_gb,min_percent)
+def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_gb:
+    if gigabytes_free < min_gb or percent_free < min_percent:
         return True
     return False
 
