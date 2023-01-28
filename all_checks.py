e895b4adafe12e1e4643af4cbbe9c7c42a56d13d
My name <me@example.com>
2023-01-28 15:51:02 +0800

    Adding a simple network connectivity check

--- all_checks.py
+++ all_checks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
+import socket
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -17,15 +18,32 @@ def check_disk_full(disk, min_gb, min_percent):
         return True
     return False
 
-def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if check_disk_full(disk="/",min_gb=2, min_percent=10):
-        print("Disk full.")
-        sys.exit(1)
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
-    print("Everything ok.")
-    sys.exit(0)
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
+def main():
+    checks=[
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full"),
+        (check_no_network, "No working network"),
+    ]
+    everything_ok= True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok= False
+
+
+    if not everything_ok:
+        sys.exit(1)        
 
 main()
